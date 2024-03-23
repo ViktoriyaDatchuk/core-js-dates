8127dd507d1c705434ddf5fbe762da5c0d6eb646
Viktoriya Datchuk <grelada@tut.by>
2024-03-23 23:24:49 +0300

    feat: finish part of tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -111,8 +111,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const diff = end - start;
+  return diff / (1000 * 60 * 60 * 24) + 1;
 }
 
 /**
@@ -132,8 +135,11 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const check = new Date(date);
+  const start = new Date(period.start);
+  const end = new Date(period.end);
+  return check >= start && check <= end;
 }
 
 /**
@@ -147,8 +153,24 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const enterValue = new Date(date);
+  const day = enterValue.getUTCDate();
+  const month = enterValue.getMonth() + 1;
+  const year = enterValue.getFullYear();
+  let hours = enterValue.getUTCHours();
+  const minutes = enterValue.getMinutes();
+  const seconds = enterValue.getSeconds();
+  let a;
+  if (hours < 12) {
+    a = 'AM';
+  } else if (hours === 12) {
+    a = 'PM';
+  } else {
+    a = 'PM';
+    hours = Math.abs(hours - 12);
+  }
+  return `${month}/${day}/${year}, ${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} ${a}`;
 }
 
 /**
@@ -163,8 +185,22 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const start = new Date(year, month - 1, 1);
+  const end = new Date(year, month, 0);
+  const days = (end - start) / (1000 * 60 * 60 * 24) + 1;
+  let weekDay = start.getDay();
+  let count = 0;
+  for (let i = 1; i <= days; i += 1) {
+    if (weekDay === 6 || weekDay === 0) {
+      count += 1;
+    }
+    weekDay += 1;
+    if (weekDay === 7) {
+      weekDay = 0;
+    }
+  }
+  return count;
 }
 
 /**
