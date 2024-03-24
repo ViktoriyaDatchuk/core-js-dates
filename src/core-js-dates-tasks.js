734255215ce15957cf024bc0c4f33c7176fc4eb8
Viktoriya Datchuk <grelada@tut.by>
2024-03-24 19:57:52 +0300

    fix: fix error with timezone

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -296,11 +296,11 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
 function getWorkSchedule(period, countWorkDays, countOffDays) {
-  const start = new Date(period.start.split('-').reverse().join('-'));
-  const end = new Date(period.end.split('-').reverse().join('-'));
-  let day = start.getUTCDate();
-  const month = start.getUTCMonth();
-  const year = start.getUTCFullYear();
+  const start = new Date(period.start.split('-').reverse().join('/'));
+  const end = new Date(period.end.split('-').reverse().join('/'));
+  let day = start.getDate();
+  const month = start.getMonth();
+  const year = start.getFullYear();
   let workDay = new Date(year, month, day);
   const result = [];
   while (workDay <= end) {
